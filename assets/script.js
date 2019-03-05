8302b0bb681d2c8137447120e5f09595216bdc85
Parker Siroishka <parkersir10@hotmail.com>
2019-03-04 18:51:16 -0700

    Working chat between users
    Basic chat implementation that includes persistant current user list and correct user being removed from users online

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,71 @@
 $(function(){
 
-    var socket = io();  
+    let socket = io();  
+    var clientUser = '';
+
+    var user = {};
+
+    socket.on('usernameCreated', function(data){
+        console.log("data here", data);
+        user.name = data.name;
+        $('#chat-title').append('<span>').text('You are '+user.name);
+    });
 
     $('form').submit(function(e){
         e.preventDefault(); // prevents page from reloading
-        socket.emit('chat message', $('#m').val());
+        console.log(user.name);
+        socket.emit('chat message', $('#m').val(), user);
         $('#m').val('');
         return false;
-    });
+    }); 
     socket.on('chat message', function(msg,user,date){
 
         if(msg != ""){
-            $('#messages').append($('<span>').text(date+' - '+user.firstName+user.lastName+': '+msg));
+            $('#messages').append($('<span>').text(date+' - '+user.name+': '+msg));
+        };
+    })
+
+    socket.on('chat message - me', function(msg,user,date){
+
+        if(msg != ""){
+            $('#messages').append('<span>'+date+' - '+user.name+': '+'<strong>'+msg+'</strong>'+'</span>');
+            
         };
     })
     socket.on('user joined - other', function(user){
-        $('#messages').append($('<span>').text(user.firstName+' '+user.lastName+' '+ ' joined'));
-        $('#online-users').append($('<li>').text(user.firstName+' '+user.lastName));
+        $('#messages').append($('<span>').text(user.name+' '+ ' joined'));
+        $('#online-users').append($('<li>').text(user.name));
     })
 
     socket.on('user joined - me', function(user){
-        $('#messages').append($('<span>').text(user.firstName+' '+user.lastName+' <me> joined'));
-        $('#online-users').append($('<li>').text(user.firstName+' '+user.lastName+' <me>'));
-    })
-    socket.on('user disconnected', function(user){
-        $('#messages').append($('<span>').text(user.firstName+' '+user.lastName+' disconnected'));
-        $('#online-users').eq(-1).remove();
+        $('#messages').append($('<span>').text(user.name+' <me> joined'));
     })
 
-    socket.on('chat command', function(msg) {
-        if(msg.slice(0,6) =="/nick"){
-            user.firstName = msg.slice(7);
-            user.lastName = "";
+    socket.on('disconnect', function(){
+        $('#messages').append($('<span>').text(user.name+' disconnected'));
+        $('#online-users').eq(-1).remove();
+    })
+    
+
+    socket.on('nameChanged', function(prevName, newName){
+        $('#messages').append($('<span>').text(prevName+' has changed their name to '+newName.name));
+        user.name = newName.name;
+        $('#chat-title').append('<span>').text('You are '+user.name);
+
+    })
+
+    socket.on('updateUsers', function(currentUsers){
+        document.getElementById("online-users").innerHTML = '';
+
+        if(currentUsers.length === 1){
+            $('#online-users').append($('<li>').text(currentUsers[0].name));
+        }
+        if (currentUsers.length > 1) {
+            currentUsers.forEach(user => {
+            $('#online-users').append($('<li>').text(user.name));
+        });
         }
     })
 
+    
 });
--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var cookie = require('cookie');
-
-let user = {};
-
-let firstNames = ["Big", "Hairy", "Slimy", "Small", "Chill", "Tall", "Short", "Blue", "Fat", "Skinny", "Bodacious", "Confident", "Beautiful"];
-let lastNames = ["Man", "Woman", "Dog", "Cat", "Cow", "Bird", "Taylor", "Buffalo", "Girl", "Boy"];
-
-function randNum(num) {
-    return Math.floor((Math.random() * num));
-}
+let express = require('express');
+let path = require('path');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let cookie = require('cookie');
 
 
-function createUserName(arr1, arr2){
-    user.firstName = arr1[randNum(13)];
-    user.lastName = arr2[randNum(10)];
 
-}
+let currentUsers = [];
+
 
 app.use('/assets', express.static('assets'));
 
@@ -29,28 +18,64 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 
-    createUserName(firstNames, lastNames);
-    
-    socket.broadcast.emit('user joined - other', user);
-    socket.emit('user joined - me', user);
+    let data = {};
+    let nickCmd = "/nick ";
+    let rgbCmd = "/nickcolor ";
+    data.name = 'user'+ randNum(10000);
+    if(currentUsers.indexOf(data.name) === -1) {
+        socket.name = data.name;
+        currentUsers.push(data);
+    } else {
+        data.name = 'user'+ randNum(10000);
+        socket.name = data.name;
+        currentUsers.push(data);
+    }
+    socket.emit('user joined - me', data);
+    socket.broadcast.emit('user joined - other', data);
 
-    socket.on('disconnect', function(user){
-        io.emit('user disconnected',user);
+    socket.emit('usernameCreated', data);
+    
+
+    
+
+    io.emit('updateUsers', currentUsers);       
+    
+    
+    
+
+    socket.on('disconnect', function(){
+        console.log(socket.name);
+        console.log(currentUsers);
+        let ind = currentUsers.findIndex(x => x.name === socket.name);
+        console.log(ind);
+        currentUsers.splice(ind,1);
+        io.emit('updateUsers', currentUsers);
     });
 
-    socket.on('chat message', function(msg){
-        if(msg.charAt(0 == "/")){
-            console.log('chat command');
-            io.emit('chat command', msg);
+    socket.on('chat message', function(msg, user){
+        
+        if(msg.indexOf(nickCmd)===0){
+            let prevName = user.name;
+            user.name = msg.substring(6);
+            let index = currentUsers.findIndex(x => x.name === prevName)
+            currentUsers[index].name = user.name;
+            io.emit('updateUsers', currentUsers);
+            socket.emit('nameChanged', prevName, user);
         }
-        var d = new Date(); // for now
-        let hours = d.getHours(); // => 9
-        let minutes = d.getMinutes(); // =>  30
-        let seconds = d.getSeconds(); // => 51
+
+        let d = new Date();
+        let hours = d.getHours();
+        let minutes = d.getMinutes();
+        let seconds = d.getSeconds();
         let date = (hours+':'+minutes+':'+seconds);
 
-        io.emit('chat message', msg,user,date);
+        socket.broadcast.emit('chat message', msg,user,date);
+        socket.emit('chat message - me', msg, user, date);
     });
+
+    function randNum(num) {
+        return Math.floor((Math.random() * num));
+    }
 });
 
 http.listen(3000, function(){
